858cc1341616828c97885ccf5fd0641c764b75e4
Majoras16 <53090319+Majoras16@users.noreply.github.com>
2020-11-07 10:28:50 +0100

    Update app.js

--- JS/app.js
+++ JS/app.js
@@ -1,29 +1,28 @@
-< script type = "text/javascript" >
-    function csv2json(csv) {
-        let j = {
-            data: []
+function csv2json(csv) {
+    let j = {
+        data: []
+    };
+    let rows = csv.split("\n");
+    let header = rows.shift();
+    rows.forEach(function (r) {
+        let cells = r.split(";");
+        j.data[j.data.length] = {
+            id: cells[0], // ID
+            nom: cells[1], // Nom du commerce
+            cat: cells[2], // Catégorie
+            desc: cells[3], // Description
+            nom_c: cells[4], // Nom (contact)
+            tel: cells[5], // Numéro de télephone */
+            email_c: cells[6], // E-mail (contact)
+            addresse: cells[7], // Adresse
+            site: cells[8], // Site Web
+            reseaux: cells[9], // Réseaux sociaux
+            services: cells[10], // Services
+            email_adm: cells[11] // E-mail (admin)
         };
-        let rows = csv.split("\n");
-        let header = rows.shift();
-        rows.forEach(function (r) {
-            let cells = r.split(";");
-            j.data[j.data.length] = {
-                id: cells[0], // ID
-                nom: cells[1], // Nom du commerce
-                cat: cells[2], // Catégorie
-                desc: cells[3], // Description
-                nom_c: cells[4], // Nom (contact)
-                tel: cells[5], // Numéro de télephone */
-                email_c: cells[6], // E-mail (contact)
-                addresse: cells[7], // Adresse
-                site: cells[8], // Site Web
-                reseaux: cells[9], // Réseaux sociaux
-                services: cells[10], // Services
-                email_adm: cells[11] // E-mail (admin)
-            };
-        });
-        return j;
-    }
+    });
+    return j;
+}
 
 function more(id, desc) {
     if (!desc || !id || typeof desc !== "string" || typeof id !== "string") {
@@ -80,4 +79,3 @@ xhr.onerror = function () {
     alert("Impossible de charger les donn\u00e9es. Essayez de recharger la page.");
 };
 xhr.send();
-</script>
